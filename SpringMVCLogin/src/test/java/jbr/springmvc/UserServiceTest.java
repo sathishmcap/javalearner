bce6e778c7d4c7b5011b0a5c87dd3104b90a3310
sathish <sathishmca.p@gmail.com>
2019-12-23 08:04:40 +0530

    initialcommit

---
+++ SpringMVCLogin/src/test/java/jbr/springmvc/UserServiceTest.java
@@ -0,0 +1,26 @@
+package jbr.springmvc;
+
+//@ContextConfiguration(locations = { "classpath:jbr/config/user-beans.xml" })
+//@RunWith(SpringJUnit4ClassRunner.class)
+public class UserServiceTest {
+
+ /*  @Autowired
+  UserService userService;
+
+  @Before
+  public void setUp() throws Exception {
+  }
+
+ @Test
+  public void testValidateUser() {
+
+    Login login = new Login();
+    login.setUsername("ranjith");
+    login.setPassword("sekar");
+
+    User user = userService.validateUser(login);
+
+    assertEquals("Ranjith", user.getFirstname());
+  }*/
+
+}
